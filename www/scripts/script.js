d930f73af4c4d9b9cb0996957a3db7da924dde8b
EvgeniyaEkimova <ekimovaevgeniya89@gmail.com>
2023-09-15 12:35:42 +0300

    делаю странцицы faq и каталог

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,13 +1,13 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
 
-  $('.header-burger').on('click', function (){
+  $('.header-burger').on('click', function () {
     $('.menu').slideToggle();
   });
 
-  $('.js-tab-link').on('click', function (event){
+  $('.js-tab-link').on('click', function (event) {
 
-    event.preventDefault ();
+    event.preventDefault();
 
     $('.js-tab-link').removeClass('active');
     $(this).addClass('active');
@@ -21,11 +21,47 @@ $(document).ready(function (){
 
 
   // FAQ
+  let prevAccordionBtn;
 
-$('.js-accordion-btn').on('click', function () {
-$(this).next().slideToggle();
+  $('.js-accordion-btn').on('click', function () {
+    if (this == prevAccordionBtn) {
+      $(this).next().slideToggle();
+    } else {
+      $(prevAccordionBtn).next().slideUp();
+      $(this).next().slideDown();
+      prevAccordionBtn = this;
+    }
 
-})
+  });
+
+
+    // каталог фильтрация
+
+    ('.js-filter-link').on('click', function (event) {
+      event.preventDefault();
+
+      $('.js-filter-link').removeClass('active');
+      $(this).addClass('active');
+
+      let dataFilter = $(this).data('filter');
+
+      if (dataFilter == 'all') {
+        $('.js-catalog-item').show();
+        return;
+      }
+
+      $('.js-catalog-item').each(function () {
+        let dataType = $(this).data('type');
+
+        if (dataType == dataFilter) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+
+    });
+
 
 
 
